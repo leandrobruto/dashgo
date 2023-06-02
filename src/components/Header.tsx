06df01b60b969dc91eeb765049b5c60019ec1880
Leandro <leandrobruto42@gmail.com>
2023-06-02 01:41:09 -0300

    feat apexcharts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
     return (
@@ -49,6 +49,31 @@ export function Header() {
 
                   <Icon as={RiSearchLine} fontSize="20" />
             </Flex>
+
+              <Flex align="center" ml="auto">
+                  <HStack
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                  >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                  </HStack>
+
+                  <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                      <Text>Leandro Bruto</Text>
+                      <Text color="gray.300" fontSize="small">
+                        leandrobruto42@gmail.com
+                      </Text>
+                    </Box>
+                    <Avatar size="md" name="Leandro Bruto" src="https://github.com/leandrobruto.png" />
+                  </Flex>
+              </Flex>
         </Flex>
     );
 }
