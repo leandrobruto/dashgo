c2074052eac61bd40d69553549f27abbccd664e0
Leandro <leandrobruto42@gmail.com>
2023-06-11 02:01:28 -0300

    feat integrate miragejs

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,47 @@
+import { Model, Factory, createServer } from 'miragejs';
+import { faker } from '@faker-js/faker';
+
+type User = {
+    name: string;
+    email: string;
+    created_at: string;
+};
+
+export function makeServer() {
+    const server = createServer({
+        models:{
+            user: Model.extend<Partial<User>>({})
+        },
+
+        factories: {
+            user: Factory.extend({
+                name(i: number) {
+                    return `User ${i + 1}`;
+                },
+                email() {
+                    return faker.internet.email();
+                },
+                createdAt() {
+                    return faker.date.recent;
+                },
+            })
+        },
+
+        seeds(server) {
+            server.createList('user', 200);
+        },
+
+        routes() {
+            this.namespace = 'api';
+            this.timing = 750;
+
+            this.get('/users');
+            this.post('/users');
+
+            this.namespace = '';
+            this.passthrough();
+        }
+    });
+
+    return server;
+}
